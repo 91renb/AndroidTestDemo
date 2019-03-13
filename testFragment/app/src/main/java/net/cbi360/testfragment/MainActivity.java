58a41423df9cbbf24116a0e150f444c4c0d0ee50
任波 <borenfocus@gmail.com>
2019-03-13 19:56:04 +0800

    更新代码

--- testFragment/app/src/main/java/net/cbi360/testfragment/MainActivity.java
+++ testFragment/app/src/main/java/net/cbi360/testfragment/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* Activity：选择 FragmentActivity 和 AppCompatActivity 都可以 */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         // 传值给Fragment
         // 页面之间的传值：Bundle是传递数据的集合
         Bundle bundle = new Bundle();
-        bundle.putString("title", "子页面标题");
+        bundle.putString("title", "标题的值");
         // 通过Bundle把参数传给Activity
         firstFragment.setArguments(bundle);
 
@@ -29,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
         // getFragmentManager();
         // 1.开启Fragment事务
         FragmentTransaction transaction = fm.beginTransaction();
-        // 2.添加（参数：布局容器FrameLayout的ID，fragment对象，动态添加时需要设置一个id/tag）
+        // 2.添加一个fragment到activity指定的控件内部
+        // （参数：布局容器FrameLayout的ID，fragment对象，动态添加时需要设置一个id/tag）
         transaction.add(R.id.content, firstFragment, "first");
         // 3.提交（对fragment进行任何操作都必须提交）
         transaction.commit();
